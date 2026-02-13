187ec9db399dfff6b675388f5fec865440209949
Milda-N <mildanar@yahoo.com>
2026-02-13 11:51:00 +0200

    Initial commit - Sales analysis project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sales Performance Analysis
+
+## Sales by Region
+
+![Sales by Region](outputs/sales_by_region.png)
---
+++ main.py
@@ -0,0 +1,40 @@
+
+import pandas as pd
+
+df = pd.read_csv("sales_data.csv")
+
+print("Failas užkrautas sėkmingai!")
+print(df.head())
+
+print("\nBendra pardavimų suma:")
+print(df["sales"].sum())
+
+print("\nVidutiniai pardavimai:")
+print(df["sales"].mean())
+
+print("\nPardavimai pagal regioną:")
+print(df.groupby("region")["sales"].sum())
+
+print("\nPardavimai pagal produktą:")
+print(df.groupby("product")["sales"].sum())
+
+import matplotlib.pyplot as plt
+
+# Pardavimai pagal regioną
+sales_by_region = df.groupby("region")["sales"].sum()
+
+# Braižome grafiką
+sales_by_region.plot(kind="bar")
+
+plt.title("Pardavimai pagal regioną")
+plt.xlabel("Regionas")
+plt.ylabel("Pardavimai")
+plt.xticks(rotation=0)
+
+plt.savefig("outputs/sales_by_region.png")
+
+plt.show()
+
+
+
+
